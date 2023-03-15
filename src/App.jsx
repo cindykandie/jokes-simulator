60271423ca03e22165fd3339fdfb5ecdb9586692
cindykandie <cindyjk6@gmail.com>
2023-03-15 20:53:35 +0300

    Add Jokes Api and display from it

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,19 @@
+import './App.css'
+import Joke from './components/Joke'
+import JokesAPI from './Data/JokesAPI'
+
 function App() {
+  const jokeElements = JokesAPI.map((joke)=>{
+    return <Joke 
+    setup = {joke.setup}
+    punchline = {joke.punchline}/>
+    }
+  )
   return (
-    <h1>bol</h1>
+    <div>
+      <h1>Its Just a Joke</h1>
+      {jokeElements}
+    </div>
   )
 }
 
