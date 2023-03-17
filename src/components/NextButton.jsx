e2f335c48aed648af7e5adb672cbfd8aca632e93
cindykandie <cindyjk6@gmail.com>
2023-03-17 13:45:46 +0300

    Add scroll view, next button and overlay

---
+++ src/components/NextButton.jsx
@@ -0,0 +1,9 @@
+function NextButton(){
+    return(
+        <div className="button-container">
+            <button className="next-btn">Another One!😁</button>
+            <span>➡▶</span>
+        </div>
+    )
+}
+export default NextButton
