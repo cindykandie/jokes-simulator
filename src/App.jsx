b855c21efe38c77866bdc8801a701adf223388b4
cindykandie <cindyjk6@gmail.com>
2023-04-16 11:46:20 +0300

    Fix Jokes input section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,12 @@ import './App.css'
 import Header from './components/Header'
 import JokeContainer from './components/JokeContainer'
 import Footer from './components/Footer'
-import JokeInput from './components/JokeInput'
 
 
 function App() {
   
   return (
     <div>
-      <JokeInput />
       <Header />
       <JokeContainer />
       <Footer />
